6401f74fbf52e107417d076733b54f365a95a24a
ThaismaraPereira <thaismarapereira@outlook.com>
2024-04-15 23:07:46 -0300

    feat: create util

---
+++ ContactsManager/Util/Enums/EmailAddressType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace ContactsManager.Util.Enums
+{
+    public enum EmailAddressType
+    {
+        [Description("Pessoal")]
+        Pessoal = 0,
+        [Description("Corporativo")]
+        Corporativo = 1,
+        [Description("Estudantil")]
+        Estudantil = 2,
+    }
+}
---
+++ ContactsManager/Util/Enums/PhoneNumberType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace ContactsManager.Util.Enums
+{
+    public enum PhoneNumberType
+    {
+        [Description("Celular")]
+        Celular = 0,
+        [Description("Residencial")]
+        Residencial = 1,
+        [Description("Comercial")]
+        Comercial = 2,
+    }
+}
